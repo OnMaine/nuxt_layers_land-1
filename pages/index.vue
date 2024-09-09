7fd58d92f770d8ec29d21a4265080ceaf38c18c7
OnMaine <volodymyr.vasylenko@futurragroup.com>
2024-09-09 11:36:46 +0200

    upd/

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="test">
-        <h1 class="original">Original page</h1>
+        <h1 class="original">Original page on <b>Site 1</b></h1>
 
-        <UI-First />
-        <UI-Second />
-        <Third/>
+        <UI-First class="first" />
+        <UI-Second class="second" />
+        <Third class="third" />
 
-        <nuxt-link to="/test">test page from Lib</nuxt-link>
+        <div>
+            <h2>Show value from composable: </h2>
+            <p>{{ test }}</p>
+        </div>
+
+        <nuxt-link to="/test" class="link">test page from Lib</nuxt-link>
     </div>
 </template>
 
@@ -16,7 +21,7 @@ definePageMeta({
  layout: 'main',
 });
 
-useTest();
+const test = useTest();
 
 log();
 </script>
@@ -24,11 +29,25 @@ log();
 <style scoped>
 .test {
     padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 
 h1 {
     color: red;
     font-size: 20px;
     text-align: center;
+}
+
+.first {
+    padding: 3px;
+    background: #35495e;
+    color: white;
+}
+
+.link {
+    margin: 20px;
 }
 </style>
